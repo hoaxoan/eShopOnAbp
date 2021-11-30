098ebbdc53ad3a10c25e0efa077f397e6ded5e01
Galip Tolga Erdem <galiperdem@gmail.com>
2021-11-30 18:34:02 +0300

    updated ocelot with kubernetes and ingress updates

--- shared/EShopOnAbp.Shared.Hosting.Gateways/EShopOnAbpSharedHostingGatewaysModule.cs
+++ shared/EShopOnAbp.Shared.Hosting.Gateways/EShopOnAbpSharedHostingGatewaysModule.cs
@@ -1,24 +1,34 @@
 ﻿using EShopOnAbp.Shared.Hosting.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
+using Ocelot.Provider.Kubernetes;
 using Ocelot.Provider.Polly;
-using Volo.Abp.AspNetCore.Mvc.UI.MultiTenancy;
 using Volo.Abp.Modularity;
 
 namespace EShopOnAbp.Shared.Hosting.Gateways
 {
     [DependsOn(
-        typeof(EShopOnAbpSharedHostingAspNetCoreModule),
-        typeof(AbpAspNetCoreMvcUiMultiTenancyModule)
+        typeof(EShopOnAbpSharedHostingAspNetCoreModule)
     )]
     public class EShopOnAbpSharedHostingGatewaysModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             var configuration = context.Services.GetConfiguration();
+            var env = context.Services.GetHostingEnvironment();
 
-            context.Services.AddOcelot(configuration)
-                .AddPolly();
+            if (env.IsStaging())
+            {
+                context.Services.AddOcelot(configuration)
+                    .AddKubernetes()
+                    .AddPolly();
+            }
+            else
+            {
+                context.Services.AddOcelot(configuration)
+                    .AddPolly();
+            }
         }
     }
 }
