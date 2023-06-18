51e2e7ceb2a940c641a79ee4cea7a0e222b3c2f1
hoaxoan <nguyendo2008@gmail.com>
2023-06-18 21:14:59 +0700

    Fix

--- shared/EShopOnAbp.Shared.Hosting.AspNetCore/SwaggerConfigurationHelper.cs
+++ shared/EShopOnAbp.Shared.Hosting.AspNetCore/SwaggerConfigurationHelper.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using Volo.Abp.Modularity;
 
 namespace EShopOnAbp.Shared.Hosting.AspNetCore;
@@ -36,6 +39,62 @@ public static class SwaggerConfigurationHelper
                 options.SwaggerDoc(apiName, new OpenApiInfo { Title = apiTitle, Version = apiVersion });
                 options.DocInclusionPredicate((docName, description) => true);
                 options.CustomSchemaIds(type => type.FullName);
-            });
+            },
+            authorizationEndpoint: "/protocol/openid-connect/auth",
+            tokenEndpoint: "/protocol/openid-connect/token"
+            );
+    }
+
+    /// <summary>
+    /// Fix Issues : Swagger Authorize With Keycloak Get Message "we are sorry"
+    /// The Issues : https://github.com/abpframework/eShopOnAbp/issues/145
+    /// </summary>
+    public static IServiceCollection AddAbpSwaggerGenWithOAuth(
+        this IServiceCollection services,
+        [NotNull] string authority,
+        [NotNull] Dictionary<string, string> scopes,
+        Action<SwaggerGenOptions> setupAction = null,
+        string authorizationEndpoint = "/connect/authorize",
+        string tokenEndpoint = "/connect/token")
+    {
+        var authorizationUrl = new Uri($"{authority.TrimEnd('/')}{authorizationEndpoint.EnsureStartsWith('/')}");
+        var tokenUrl = new Uri($"{authority.TrimEnd('/')}{tokenEndpoint.EnsureStartsWith('/')}");
+
+        return services
+            .AddAbpSwaggerGen()
+            .AddSwaggerGen(
+                options =>
+                {
+                    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.OAuth2,
+                        Flows = new OpenApiOAuthFlows
+                        {
+                            AuthorizationCode = new OpenApiOAuthFlow
+                            {
+                                AuthorizationUrl = authorizationUrl,
+                                Scopes = scopes,
+                                TokenUrl = tokenUrl
+                            }
+                        }
+                    });
+
+                    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                            {
+                                new OpenApiSecurityScheme
+                                {
+                                    Reference = new OpenApiReference
+                                    {
+                                        Type = ReferenceType.SecurityScheme,
+                                        Id = "oauth2"
+                                    }
+                                },
+                                Array.Empty<string>()
+                            }
+                    });
+
+                    setupAction?.Invoke(options);
+                });
     }
 }
